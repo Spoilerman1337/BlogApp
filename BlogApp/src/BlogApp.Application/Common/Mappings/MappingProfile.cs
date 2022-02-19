5c4bda68a2145988e6c77e313a269cf6a5e601e4
Maxim Toropygin <mrcatman1337@gmail.com>
2022-02-19 16:49:16 +0300

    added mapping

---
+++ BlogApp/src/BlogApp.Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using BlogApp.Application.Common.Interfaces;
+using System.Reflection;
+
+namespace BlogApp.Application.Common.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            ApplyMappingsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+            .Where(type => type.GetInterfaces()
+            .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapFrom<>)))
+            .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
