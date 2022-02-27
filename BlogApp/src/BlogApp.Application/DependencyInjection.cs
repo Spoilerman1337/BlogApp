7d19ae0c2bfdba71c7c61e4c3e2bea6fe50d7ca5
Maxim Toropygin <mrcatman1337@gmail.com>
2022-02-27 13:47:13 +0300

    added application DI

---
+++ BlogApp/src/BlogApp.Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using BlogApp.Application.Common.Interfaces;
+using BlogApp.Application.Common.Mappings;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BlogApp.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddAutoMapper(config =>
+        {
+            config.AddProfile(new MappingProfile(Assembly.GetExecutingAssembly()));
+            config.AddProfile(new MappingProfile(typeof(IBlogDbContext).Assembly));
+        });
+
+        return services;
+    }
+}
