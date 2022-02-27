3a6563bf2ec953753813180342c503f806fd4bcc
Maxim Toropygin <mrcatman1337@gmail.com>
2022-02-27 13:54:07 +0300

    added di in api

--- BlogApp/src/BlogApp.Presentation/Program.cs
+++ BlogApp/src/BlogApp.Presentation/Program.cs
@@ -1,4 +1,14 @@
+using BlogApp.Application;
+using BlogApp.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+ConfigurationManager configuration = builder.Configuration;
+
+// Add services to the container.
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
