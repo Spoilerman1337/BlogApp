bd7fbe159c54712dcae05f6382041eb05def1e21
Maxim Toropygin <mrcatman1337@gmail.com>
2022-04-09 12:31:24 +0300

    api controller fix, availible to use user identity in controllers

---
+++ BlogApp/src/BlogApp.Presentation/Controllers/ApiControllerBase.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BlogApp.Presentation.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ApiControllerBase : ControllerBase
+{
+    private ISender _sender = null;
+    protected ISender Sender => _sender ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+
+    internal Guid UserId => !User.Identity.IsAuthenticated
+            ? Guid.Empty
+            : Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+}
