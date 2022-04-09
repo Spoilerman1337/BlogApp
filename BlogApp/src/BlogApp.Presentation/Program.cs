7cd9460c916c0965a406901a8c823ce983761d49
Maxim Toropygin <mrcatman1337@gmail.com>
2022-04-09 12:48:22 +0300

    Added CORS, and controllers middleware

--- BlogApp/src/BlogApp.Presentation/Program.cs
+++ BlogApp/src/BlogApp.Presentation/Program.cs
@@ -8,9 +8,27 @@ ConfigurationManager configuration = builder.Configuration;
 // Add services to the container.
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(configuration);
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
