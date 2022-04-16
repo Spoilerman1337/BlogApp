789fbdcb5dbc81279c08cf128f0f3e10dfc0847e
Maxim Toropygin <mrcatman1337@gmail.com>
2022-04-16 17:42:41 +0300

    validator fixes

---
+++ BlogApp/src/BlogApp.Application/Comments/Queries/GetUsersComments/GetUsersCommentsQueryValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace BlogApp.Application.Comments.Queries.GetUsersComments;
+
+public class GetUsersCommentsValidator : AbstractValidator<GetUsersCommentsQuery>
+{
+    public GetUsersCommentsValidator()
+    {
+        RuleFor(getCommentQuery => getCommentQuery.UserId).NotEqual(Guid.Empty);
+    }
+}
