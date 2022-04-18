92b09e0d2ff751fc51f22b2fc1def084fde2f0ad
MT <mrcatman1337@gmail.com>
2022-04-18 13:39:01 +0300

    added get post by comment query

---
+++ BlogApp/src/BlogApp.Application/Posts/Queries/GetPostByComment/GetPostByCommentValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace BlogApp.Application.Posts.Queries.GetPostByComment;
+
+public class GetPostByCommentValidator : AbstractValidator<GetPostByCommentQuery>
+{
+    public GetPostByCommentValidator()
+    {
+        RuleFor(getPostQuery => getPostQuery.CommentId).NotEqual(Guid.Empty);
+    }
+}
