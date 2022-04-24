ee415191df5550441877540b1437272a95a5f975
Maxim Toropygin <mrcatman1337@gmail.com>
2022-04-24 12:14:21 +0300

    added create user command

---
+++ BlogApp.Auth/src/BlogApp.Auth.Application/Users/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using BlogApp.Auth.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlogApp.Auth.Application.Users.Commands.CreateUser;
+
+public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, IdentityResult>
+{
+    private readonly UserManager<AppUser> _userManager;
+
+    public CreateUserCommandHandler(UserManager<AppUser> userManager) => _userManager = userManager;
+
+    public async Task<IdentityResult> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = new AppUser
+        {
+            Id = Guid.NewGuid(),
+            UserName = request.UserName,
+            Email = request.Email,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Patronymic = request.Patronymic
+        };
+
+        var result = await _userManager.CreateAsync(user, request.Password);
+
+        return result;
+    }
+}
---
+++ BlogApp.Auth/src/BlogApp.Auth.Application/Users/Commands/CreateUser/CreateUserCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace BlogApp.Auth.Application.Users.Commands.CreateUser;
+
+public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+{
+    public CreateUserCommandValidator()
+    {
+        RuleFor(createUserCommand => createUserCommand.Id).NotEqual(Guid.Empty);
+        RuleFor(createUserCommand => createUserCommand.UserName).Length(6, 25);
+        RuleFor(createUserCommand => createUserCommand.Password).MinimumLength(6);
+        RuleFor(createUserCommand => createUserCommand.Email).NotEmpty().EmailAddress();
+    }
+}
