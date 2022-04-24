727b78c814325fa4ccc3856ba000b88ae0997791
Maxim Toropygin <mrcatman1337@gmail.com>
2022-04-24 22:26:43 +0300

    create user command fix

--- BlogApp.Auth/src/BlogApp.Auth.Application/Users/Commands/CreateUser/CreateUserCommand.cs
+++ BlogApp.Auth/src/BlogApp.Auth.Application/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -5,7 +5,6 @@ namespace BlogApp.Auth.Application.Users.Commands.CreateUser;
 
 public class CreateUserCommand : IRequest<IdentityResult>
 {
-    public Guid Id { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
     public string Email { get; set; }
