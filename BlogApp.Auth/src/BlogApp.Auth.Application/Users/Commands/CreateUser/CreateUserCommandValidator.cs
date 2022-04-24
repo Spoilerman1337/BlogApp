6587e2b56363e28346b0542db076a83bb8af87d1
Maxim Toropygin <mrcatman1337@gmail.com>
2022-04-24 22:36:05 +0300

    create user command validator fix

--- BlogApp.Auth/src/BlogApp.Auth.Application/Users/Commands/CreateUser/CreateUserCommandValidator.cs
+++ BlogApp.Auth/src/BlogApp.Auth.Application/Users/Commands/CreateUser/CreateUserCommandValidator.cs
@@ -6,9 +6,12 @@ public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
 {
     public CreateUserCommandValidator()
     {
-        RuleFor(createUserCommand => createUserCommand.Id).NotEqual(Guid.Empty);
         RuleFor(createUserCommand => createUserCommand.UserName).Length(6, 25);
         RuleFor(createUserCommand => createUserCommand.Password).MinimumLength(6);
         RuleFor(createUserCommand => createUserCommand.Email).NotEmpty().EmailAddress();
+
+        RuleFor(createUserCommand => createUserCommand.FirstName).MaximumLength(25);
+        RuleFor(createUserCommand => createUserCommand.LastName).MaximumLength(25);
+        RuleFor(createUserCommand => createUserCommand.Patronymic).MaximumLength(25);
     }
 }
