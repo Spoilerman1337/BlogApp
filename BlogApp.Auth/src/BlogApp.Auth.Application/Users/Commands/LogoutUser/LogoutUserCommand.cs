73829e7cb47a1c8b5cb5ae460055048ab834eccf
Maxim Toropygin <mrcatman1337@gmail.com>
2022-05-03 01:13:30 +0300

    logout user command fix

--- BlogApp.Auth/src/BlogApp.Auth.Application/Users/Commands/LogoutUser/LogoutUserCommand.cs
+++ BlogApp.Auth/src/BlogApp.Auth.Application/Users/Commands/LogoutUser/LogoutUserCommand.cs
@@ -2,8 +2,4 @@
 
 namespace BlogApp.Auth.Application.Users.Commands.LogoutUser;
 
-public class LogoutUserCommand : IRequest
-{
-    public string UserName { get; set; }
-    public string Password { get; set; }
-}
+public class LogoutUserCommand : IRequest { }
