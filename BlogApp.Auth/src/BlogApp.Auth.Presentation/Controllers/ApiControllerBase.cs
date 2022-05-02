97b97e506470d32b498f0d55241695a8f7fb6e40
Maxim Toropygin <mrcatman1337@gmail.com>
2022-05-03 01:15:43 +0300

    base controller fix

--- BlogApp.Auth/src/BlogApp.Auth.Presentation/Controllers/ApiControllerBase.cs
+++ BlogApp.Auth/src/BlogApp.Auth.Presentation/Controllers/ApiControllerBase.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace BlogApp.Auth.Presentation.Controllers;
 
 [ApiController]
-[Route("api/[controller]/[action]")]
+[Route("api/[controller]")]
 public class ApiControllerBase : Controller
 {
     private ISender _sender = null;
