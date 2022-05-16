8875d613505fc5f2de9ec1f7ec396ed03d05e938
MT <mrcatman1337@gmail.com>
2022-05-16 10:24:27 +0300

    auth validator di fix

--- BlogApp.Auth/src/BlogApp.Auth.Application/DependencyInjection.cs
+++ BlogApp.Auth/src/BlogApp.Auth.Application/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using BlogApp.Auth.Application.Common.Interfaces;
 using BlogApp.Auth.Application.Common.Mappings;
 using BlogApp.Auth.Domain.Entities;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
@@ -13,6 +14,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
         services.AddAutoMapper(config =>
         {
             config.AddProfile(new MappingProfile(Assembly.GetExecutingAssembly()));
