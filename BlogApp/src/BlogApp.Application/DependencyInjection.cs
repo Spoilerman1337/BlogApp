1f459a724da9384ad2d789aaedd489317426a2dd
MT <mrcatman1337@gmail.com>
2022-05-16 10:20:47 +0300

    di validator fix

--- BlogApp/src/BlogApp.Application/DependencyInjection.cs
+++ BlogApp/src/BlogApp.Application/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using BlogApp.Application.Common.Behaviors;
 using BlogApp.Application.Common.Interfaces;
 using BlogApp.Application.Common.Mappings;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
@@ -12,6 +13,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
         services.AddAutoMapper(config =>
         {
             config.AddProfile(new MappingProfile(Assembly.GetExecutingAssembly()));
