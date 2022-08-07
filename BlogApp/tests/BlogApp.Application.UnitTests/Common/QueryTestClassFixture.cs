7a71a4ad187090e8bd61bb6602bb2adc293aea8f
Maxim Toropygin <mrcatman1337@gmail.com>
2022-08-07 10:17:05 +0300

    added query fixture

---
+++ BlogApp/tests/BlogApp.Application.UnitTests/Common/QueryTestClassFixture.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using BlogApp.Application.Common.Interfaces;
+using BlogApp.Application.Common.Mappings;
+using BlogApp.Infrastructure.Persistance;
+using System.Reflection;
+using Xunit;
+
+namespace BlogApp.Application.UnitTests.Common;
+
+public class QueryTestClassFixture : IDisposable
+{
+    public readonly BlogDbContext _context;
+    public readonly IMapper _mapper;
+
+    public QueryTestClassFixture()
+    {
+        _context = BlogAppContextFactory.Create();
+
+        var mapperConfiguration = new MapperConfiguration(config =>
+        {
+            config.AddProfile(new MappingProfile(Assembly.GetExecutingAssembly()));
+            config.AddProfile(new MappingProfile(typeof(IBlogDbContext).Assembly));
+        });
+        _mapper = mapperConfiguration.CreateMapper();
+    }
+
+    public void Dispose()
+    {
+        BlogAppContextFactory.Destroy(_context);
+    }
+}
+
+[CollectionDefinition("QueryCollection")]
+public class QueryCollection : ICollectionFixture<QueryTestClassFixture> { }
