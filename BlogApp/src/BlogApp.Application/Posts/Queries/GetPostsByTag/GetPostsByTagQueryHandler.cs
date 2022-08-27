cf271f45358508b328d1a2ff880422c9a3d7af74
Maxim Toropygin <mrcatman1337@gmail.com>
2022-08-27 10:43:48 +0300

    get posts by tag fix

--- BlogApp/src/BlogApp.Application/Posts/Queries/GetPostsByTag/GetPostsByTagQueryHandler.cs
+++ BlogApp/src/BlogApp.Application/Posts/Queries/GetPostsByTag/GetPostsByTagQueryHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using BlogApp.Application.Common.Exceptions;
 using BlogApp.Application.Common.Interfaces;
 using BlogApp.Application.Posts.Queries.GetPostsByTag.Models;
 using MediatR;
@@ -16,6 +17,9 @@ public class GetPostsByTagQueryHandler : IRequestHandler<GetPostsByTagQuery, Lis
 
     public async Task<List<GetPostsByTagDto>> Handle(GetPostsByTagQuery request, CancellationToken cancellationToken)
     {
+        if (!_dbContext.Tags.Select(c => c.Id).Contains(request.TagId))
+            throw new NotFoundException(nameof(GetPostsByTagQueryHandler), request.TagId);
+
         return await _dbContext.Posts.Where(c => c.Tags.Select(c => c.Id).Contains(request.TagId))
                                      .OrderBy(c => c.Id)
                                      .ProjectTo<GetPostsByTagDto>(_mapper.ConfigurationProvider)
