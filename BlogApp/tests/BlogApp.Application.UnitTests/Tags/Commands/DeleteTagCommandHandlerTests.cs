58586652a69e058caab80f7078e7c4a0b98e19a1
Maxim Toropygin <mrcatman1337@gmail.com>
2022-08-28 10:56:57 +0300

    tag command tests

---
+++ BlogApp/tests/BlogApp.Application.UnitTests/Tags/Commands/DeleteTagCommandHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using BlogApp.Application.Common.Exceptions;
+using BlogApp.Application.Tags.Commands.DeleteTag;
+using BlogApp.Application.UnitTests.Common;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace BlogApp.Application.UnitTests.Tags.Commands;
+
+public class DeleteTagCommandHandlerTests : TestCommandBase
+{
+    [Fact]
+    public async Task DeleteTagCommandHandler_Success()
+    {
+        //Arrange
+        var handler = new DeleteTagCommandHandler(Context);
+
+        //Act
+        await handler.Handle(
+            new DeleteTagCommand
+            {
+                Id = BlogAppContextFactory.ToBeDeletedTagId
+            },
+            CancellationToken.None
+        );
+
+        //Assert
+        (await Context.Tags.SingleOrDefaultAsync(c => c.Id == BlogAppContextFactory.ToBeDeletedTagId)).Should().BeNull();
+    }
+
+    [Fact]
+    public async Task DeleteTagCommandHandler_WrongIdShouldThrow()
+    {
+        //Arrange
+        var handler = new DeleteTagCommandHandler(Context);
+
+        //Act
+
+        //Assert
+        await Assert.ThrowsAsync<NotFoundException>(async () => await handler.Handle(
+            new DeleteTagCommand
+            {
+                Id = Guid.NewGuid()
+            },
+            CancellationToken.None
+        ));
+    }
+}
