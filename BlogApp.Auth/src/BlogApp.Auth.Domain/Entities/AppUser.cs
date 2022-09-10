4a51920150170aef56a7e4bdcac8a4aa349d2d74
Maxim Toropygin <mrcatman1337@gmail.com>
2022-09-10 12:15:33 +0300

    appuser name are not required

--- BlogApp.Auth/src/BlogApp.Auth.Domain/Entities/AppUser.cs
+++ BlogApp.Auth/src/BlogApp.Auth.Domain/Entities/AppUser.cs
@@ -4,7 +4,7 @@ namespace BlogApp.Auth.Domain.Entities;
 
 public class AppUser : IdentityUser<Guid>
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Patronymic { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Patronymic { get; set; }
 }
--- BlogApp.Auth/src/BlogApp.Auth.Infrastructure/Migrations/20220910091433_user-names-are-not-required.Designer.cs
+++ BlogApp.Auth/src/BlogApp.Auth.Infrastructure/Migrations/20220910091433_user-names-are-not-required.Designer.cs
@@ -4,6 +4,7 @@ using BlogApp.Auth.Infrastructure.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlogApp.Auth.Infrastructure.Migrations
 {
     [DbContext(typeof(BlogAuthDbContext))]
-    partial class BlogAuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220910091433_user-names-are-not-required")]
+    partial class usernamesarenotrequired
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
