07034d5cdf831df14f1a8a3064232757143b5eb8
Maxim Toropygin <mrcatman1337@gmail.com>
2022-09-14 09:38:16 +0300

    user query unit tests

---
+++ BlogApp.Auth/tests/BlogApp.Auth.Application.UnitTests/Users/Queries/GetUserQueryHandlerTests.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using BlogApp.Auth.Application.UnitTests.Common;
+using BlogApp.Auth.Application.Users.Queries.GetUser;
+using BlogApp.Auth.Application.Users.Queries.GetUser.Models;
+using BlogApp.Auth.Domain.Entities;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BlogApp.Auth.Application.UnitTests.Users.Queries;
+
+[Collection("QueryCollection")]
+public class GetUserQueryHandlerTests
+{
+    private readonly UserManager<AppUser> _userManager;
+    private readonly IMapper _mapper;
+
+    public GetUserQueryHandlerTests(QueryTestClassFixture fixture)
+    {
+        _userManager = fixture._userManager;
+        _mapper = fixture._mapper;
+    }
+
+    [Fact]
+    public async Task GetUserQueryHandler_Success()
+    {
+        //Arrange
+        var handler = new GetUserQueryHandler(_userManager, _mapper);
+
+        //Act
+        var result = await handler.Handle(
+            new GetUserQuery
+            {
+                Id = Guid.Parse("FDAE39CF-B55E-47BC-9904-00DF3CFD6FAD")
+            },
+            CancellationToken.None
+        );
+
+        //Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<GetUserDto>();
+    }
+
+    [Fact]
+    public async Task GetUserQueryHandler_Fail()
+    {
+        //Arrange
+        var handler = new GetUserQueryHandler(_userManager, _mapper);
+
+        //Act
+        var result = await handler.Handle(
+            new GetUserQuery
+            {
+                Id = Guid.NewGuid()
+            },
+            CancellationToken.None
+        );
+
+        //Assert
+        result.Should().BeNull();
+    }
+}
---
+++ BlogApp.Auth/tests/BlogApp.Auth.Application.UnitTests/Users/Queries/GetUsersQueryHandlerTests.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using BlogApp.Auth.Application.UnitTests.Common;
+using BlogApp.Auth.Application.UnitTests.Mocks;
+using BlogApp.Auth.Application.Users.Queries.GetUsers;
+using BlogApp.Auth.Application.Users.Queries.GetUsers.Models;
+using BlogApp.Auth.Domain.Entities;
+using BlogApp.Auth.Infrastructure.Persistance;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BlogApp.Auth.Application.UnitTests.Users.Queries;
+
+[Collection("QueryCollection")]
+public class GetUsersQueryHandlerTests
+{
+    private readonly UserManager<AppUser> _userManager;
+    private readonly IMapper _mapper;
+
+    public GetUsersQueryHandlerTests(QueryTestClassFixture fixture)
+    {
+        _userManager = fixture._userManager;
+        _mapper = fixture._mapper;
+    }
+
+    [Fact]
+    public async Task GetUsersQueryHandler_Success()
+    {
+        //Arrange
+        var handler = new GetUsersQueryHandler(_userManager, _mapper);
+
+        //Act
+        var result = await handler.Handle(
+            new GetUsersQuery(),
+            CancellationToken.None
+        );
+
+        //Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<List<GetUsersDto>>();
+    }
+
+    [Fact]
+    public async Task GetUsersQueryHandler_NoUsersFail()
+    {
+        //Arrange
+        var options = new DbContextOptionsBuilder<BlogAuthDbContext>()
+               .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+        var context = new BlogAuthDbContext(options);
+        var userManager = UserManagerMock.Create(context);
+        var handler = new GetUsersQueryHandler(userManager.Object, _mapper);
+
+        //Act
+        var result = await handler.Handle(
+            new GetUsersQuery(),
+            CancellationToken.None
+        );
+
+        //Assert
+        result.Should().NotBeNull();
+        result.Should().BeEmpty();
+        result.Should().BeOfType<List<GetUsersDto>>();
+    }
+}
