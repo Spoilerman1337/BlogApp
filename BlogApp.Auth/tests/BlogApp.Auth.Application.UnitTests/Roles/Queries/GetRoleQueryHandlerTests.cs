28ad82862e734fe07fcf5afd860c93fa43ef9904
Maxim Toropygin <mrcatman1337@gmail.com>
2022-09-15 10:19:11 +0300

    roles query tests

---
+++ BlogApp.Auth/tests/BlogApp.Auth.Application.UnitTests/Roles/Queries/GetRoleQueryHandlerTests.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using BlogApp.Auth.Application.Roles.Queries.GetRole;
+using BlogApp.Auth.Application.Roles.Queries.GetRole.Models;
+using BlogApp.Auth.Application.UnitTests.Common;
+using BlogApp.Auth.Domain.Entities;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BlogApp.Auth.Application.UnitTests.Roles.Queries;
+
+[Collection("QueryCollection")]
+public class GetRoleQueryHandlerTests
+{
+    private readonly RoleManager<AppRole> _roleManager;
+    private readonly IMapper _mapper;
+
+    public GetRoleQueryHandlerTests(QueryTestClassFixture fixture)
+    {
+        _roleManager = fixture._roleManager;
+        _mapper = fixture._mapper;
+    }
+
+    [Fact]
+    public async Task GetRoleQueryHandler_Success()
+    {
+        //Arrange
+        var handler = new GetRoleQueryHandler(_roleManager, _mapper);
+
+        //Act
+        var result = await handler.Handle(
+            new GetRoleQuery
+            {
+                Id = Guid.Parse("5C29FCAB-1AAD-4EDD-8D75-3770D98D651D")
+            },
+            CancellationToken.None
+        );
+
+        //Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<GetRoleDto>();
+    }
+
+    [Fact]
+    public async Task GetRoleQueryHandler_Fail()
+    {
+        //Arrange
+        var handler = new GetRoleQueryHandler(_roleManager, _mapper);
+
+        //Act
+        var result = await handler.Handle(
+            new GetRoleQuery
+            {
+                Id = Guid.NewGuid()
+            },
+            CancellationToken.None
+        );
+
+        //Assert
+        result.Should().BeNull();
+    }
+}
