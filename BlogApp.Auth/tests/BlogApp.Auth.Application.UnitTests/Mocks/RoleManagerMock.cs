35251d65b37bec9ae93ac72a2093fe48d92ebc0c
Maxim Toropygin <mrcatman1337@gmail.com>
2022-09-17 12:48:12 +0300

    mock cleanups, new method setups

--- BlogApp.Auth/tests/BlogApp.Auth.Application.UnitTests/Mocks/RoleManagerMock.cs
+++ BlogApp.Auth/tests/BlogApp.Auth.Application.UnitTests/Mocks/RoleManagerMock.cs
@@ -16,10 +16,17 @@ public class RoleManagerMock
         roleManager.Object.RoleValidators.Add(new RoleValidator<AppRole>());
 
         roleManager.Setup(x => x.Roles).Returns(context.Roles);
-        roleManager.Setup(x => x.FindByIdAsync(It.IsAny<string>())).ReturnsAsync((string roleId) => context.Roles.Find(Guid.Parse(roleId)));
-        roleManager.Setup(x => x.DeleteAsync(It.IsAny<AppRole>())).ReturnsAsync(IdentityResult.Success).Callback<AppRole>((x) => context.Roles.Remove(x).Context.SaveChanges()); ;
-        roleManager.Setup(x => x.CreateAsync(It.IsAny<AppRole>())).ReturnsAsync(IdentityResult.Success).Callback<AppRole>((x) => context.Roles.Add(x).Context.SaveChanges());
-        roleManager.Setup(x => x.UpdateAsync(It.IsAny<AppRole>())).ReturnsAsync(IdentityResult.Success).Callback<AppRole>((x) => context.SaveChanges()); ;
+        roleManager.Setup(x => x.FindByIdAsync(It.IsAny<string>()))
+            .ReturnsAsync((string roleId) => context.Roles.Find(Guid.Parse(roleId)));
+        roleManager.Setup(x => x.DeleteAsync(It.IsAny<AppRole>()))
+            .ReturnsAsync(IdentityResult.Success)
+            .Callback<AppRole>((x) => context.Roles.Remove(x).Context.SaveChanges()); ;
+        roleManager.Setup(x => x.CreateAsync(It.IsAny<AppRole>()))
+            .ReturnsAsync(IdentityResult.Success)
+            .Callback<AppRole>((x) => context.Roles.Add(x).Context.SaveChanges());
+        roleManager.Setup(x => x.UpdateAsync(It.IsAny<AppRole>()))
+            .ReturnsAsync(IdentityResult.Success)
+            .Callback<AppRole>((x) => context.SaveChanges()); ;
 
         return roleManager;
     }
--- BlogApp.Auth/tests/BlogApp.Auth.Application.UnitTests/Mocks/UserManagerMock.cs
+++ BlogApp.Auth/tests/BlogApp.Auth.Application.UnitTests/Mocks/UserManagerMock.cs
@@ -1,8 +1,11 @@
 ﻿using BlogApp.Auth.Domain.Entities;
 using BlogApp.Auth.Infrastructure.Persistance;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BlogApp.Auth.Application.UnitTests.Mocks;
 
@@ -16,11 +19,28 @@ public static class UserManagerMock
         userManager.Object.UserValidators.Add(new UserValidator<AppUser>());
         userManager.Object.PasswordValidators.Add(new PasswordValidator<AppUser>());
 
-        userManager.Setup(x => x.Users).Returns(context.Users);
-        userManager.Setup(x => x.FindByIdAsync(It.IsAny<string>())).ReturnsAsync((string userId) => context.Users.Find(Guid.Parse(userId)));
-        userManager.Setup(x => x.DeleteAsync(It.IsAny<AppUser>())).ReturnsAsync(IdentityResult.Success).Callback<AppUser>((x) => context.Users.Remove(x).Context.SaveChanges());
-        userManager.Setup(x => x.CreateAsync(It.IsAny<AppUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success).Callback<AppUser, string>((x, y) => context.Users.Add(x).Context.SaveChanges());
-        userManager.Setup(x => x.UpdateAsync(It.IsAny<AppUser>())).ReturnsAsync(IdentityResult.Success).Callback<AppUser>((x) => context.SaveChanges());
+        userManager.Setup(x => x.Users)
+            .Returns(context.Users);
+
+        userManager.Setup(x => x.FindByIdAsync(It.IsAny<string>()))
+            .ReturnsAsync((string userId) => context.Users.Find(Guid.Parse(userId)));
+        userManager.Setup(x => x.GetRolesAsync(It.IsAny<AppUser>()))
+            .ReturnsAsync((AppUser user) => context.Roles.Where(c => context.UserRoles.Where(c => c.UserId == user.Id).Select(c => c.RoleId).Contains(c.Id)).Select(c => c.Name).ToList());
+        userManager.Setup(x => x.RemoveFromRolesAsync(It.IsAny<AppUser>(), It.IsAny<IEnumerable<string>>()))
+            .ReturnsAsync(IdentityResult.Success)
+            .Callback<AppUser, IEnumerable<string>>((x, y) => context.UserRoles.Remove(context.UserRoles.Where(a => a.UserId == x.Id && a.RoleId.Equals(context.Roles.Where(c => c.Name == y.First()).First().Id)).First()).Context.SaveChanges());
+        userManager.Setup(x => x.AddToRoleAsync(It.IsAny<AppUser>(), It.IsAny<string>()))
+            .ReturnsAsync(IdentityResult.Success)
+            .Callback<AppUser, string>((x, y) => context.UserRoles.Add(new IdentityUserRole<Guid> { RoleId = context.Roles.Where(c => c.Name == y).First().Id, UserId = x.Id }).Context.SaveChanges());
+        userManager.Setup(x => x.DeleteAsync(It.IsAny<AppUser>()))
+            .ReturnsAsync(IdentityResult.Success)
+            .Callback<AppUser>((x) => context.Users.Remove(x).Context.SaveChanges());
+        userManager.Setup(x => x.CreateAsync(It.IsAny<AppUser>(), It.IsAny<string>()))
+            .ReturnsAsync(IdentityResult.Success)
+            .Callback<AppUser, string>((x, y) => context.Users.Add(x).Context.SaveChanges());
+        userManager.Setup(x => x.UpdateAsync(It.IsAny<AppUser>()))
+            .ReturnsAsync(IdentityResult.Success)
+            .Callback<AppUser>((x) => context.SaveChanges());
 
         return userManager;
     }
