23371406ec14b7c89cfa270868ba9eea5fdacf9e
Maxim Toropygin <mrcatman1337@gmail.com>
2022-09-18 10:46:39 +0300

    dbcontext interface dbset fix

--- BlogApp/src/BlogApp.Application/Common/Interfaces/IBlogDbContext.cs
+++ BlogApp/src/BlogApp.Application/Common/Interfaces/IBlogDbContext.cs
@@ -8,6 +8,7 @@ public interface IBlogDbContext
     public DbSet<Post> Posts { get; set; }
     public DbSet<Comment> Comments { get; set; }
     public DbSet<Tag> Tags { get; set; }
+    public DbSet<VotePost> VotePosts { get; set; }
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
