c633e2d7c1dc1b3e87ad051d15d34508db3a5240
Maxim Toropygin <mrcatman1337@gmail.com>
2022-09-18 12:21:22 +0300

    upvote post dto fix [2]

--- BlogApp/src/BlogApp.Application/VotePosts/Commands/UpvotePost/Models/UpvotePostDto.cs
+++ BlogApp/src/BlogApp.Application/VotePosts/Commands/UpvotePost/Models/UpvotePostDto.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BlogApp.Application.VotePosts.Commands.UpvotePost.Models;
 
-public class UpvotePostDto : IMapFrom<UpvotePostDto>
+public class UpvotePostDto : IMapFrom<UpvotePostCommand>
 {
     [Required]
     public Guid PostId { get; set; }
