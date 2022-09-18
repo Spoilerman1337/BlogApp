90a4c38c81a79c5c02c902b1579e421f13157c84
Maxim Toropygin <mrcatman1337@gmail.com>
2022-09-18 12:16:44 +0300

    new constructor for not found exception with 2-key composite keys

--- BlogApp/src/BlogApp.Application/Common/Exceptions/NotFoundException.cs
+++ BlogApp/src/BlogApp.Application/Common/Exceptions/NotFoundException.cs
@@ -3,4 +3,5 @@
 public class NotFoundException : Exception
 {
     public NotFoundException(string name, object key) : base($"Entity \"{name}\" ({key}) not found.") { }
+    public NotFoundException(string name, object key1, object key2) : base($"Entity \"{name}\" ({key1};{key2}) not found.") { }
 }
