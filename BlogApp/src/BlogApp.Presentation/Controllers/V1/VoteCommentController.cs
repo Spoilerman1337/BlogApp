5bd5d3f0c482d52d761701e406ee66a1bfd3dc8f
Maxim Toropygin <mrcatman1337@gmail.com>
2022-09-19 21:42:22 +0300

    vote comment controller

---
+++ BlogApp/src/BlogApp.Presentation/Controllers/V1/VoteCommentController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using BlogApp.Application.VoteComments.Commands.ChangeVoteComment;
+using BlogApp.Application.VotePosts.Commands.ChangeVotePost;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Presentation.Controllers.V1;
+
+[ApiVersion("1.0")]
+[Produces("application/json")]
+public class VoteCommentController : ApiControllerBase
+{
+    private readonly IMapper _mapper;
+
+    public VoteCommentController(IMapper mapper) => _mapper = mapper;
+
+    /// <summary>Changes user's vote on comment on opposite</summary>
+    /// <remarks>
+    /// Sample request:
+    /// PUT /voteComment/b5c0a7ae-762d-445d-be15-b59232b19383
+    /// </remarks>
+    /// <param name="commentId">GUID ID of a commentId</param>
+    /// <response code="204">Success</response>
+    /// <response code="401">If unauthorized</response>
+    [HttpPut("{commentId}")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<ActionResult> ChangeVoteComment([FromRoute] Guid commentId)
+    {
+        await Sender.Send(new ChangeVoteCommentCommand
+        {
+            CommentId = commentId,
+            UserId = UserId
+        });
+
+        return NoContent();
+    }
+}
