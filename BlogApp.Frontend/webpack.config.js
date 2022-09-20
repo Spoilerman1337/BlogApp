d782c81f3a9b3a82c4896cd6f06da8fa577c887b
Maxim Toropygin <mrcatman1337@gmail.com>
2022-09-20 21:31:05 +0300

    babel install

--- BlogApp.Frontend/webpack.config.js
+++ BlogApp.Frontend/webpack.config.js
@@ -12,6 +12,20 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    module: {
+        rules: [
+            {
+              test: /\.m?js$/,
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env']
+                }
+              }
+            }
+          ]
+    },
     plugin: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
