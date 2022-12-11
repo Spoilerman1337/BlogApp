a07fcd74ff69bdc5ad2434525e4c80b19c0a00e5
Maxim Toropygin <mrcatman1337@gmail.com>
2022-12-11 16:08:50 +0300

    added interface for sorting queries

---
+++ BlogApp/src/BlogApp.Application/Common/Interfaces/ISortableQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogApp.Application.Common.Interfaces;
+
+public interface ISortableQuery
+{
+    public DateTime? From { get; set; }
+    public DateTime? To { get; set; }
+    public int? PageAmount { get; set; }
+    public int? Page { get; set; }
+}
