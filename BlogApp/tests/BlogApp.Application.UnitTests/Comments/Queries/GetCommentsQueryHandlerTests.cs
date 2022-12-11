e95fb9b88f1c724baf1db4bca01add5757327336
Maxim Toropygin <mrcatman1337@gmail.com>
2022-12-11 16:09:32 +0300

    added tests to sorting getcomments query

--- BlogApp/tests/BlogApp.Application.UnitTests/Comments/Queries/GetCommentsQueryHandlerTests.cs
+++ BlogApp/tests/BlogApp.Application.UnitTests/Comments/Queries/GetCommentsQueryHandlerTests.cs
@@ -39,6 +39,72 @@ public class GetCommentsQueryHandlerTests
     }
 
     [Fact]
+    public async Task GetCommentsQueryHandlerDateFilter_SuccessNotFound()
+    {
+        //Arrange
+        var handler = new GetCommentsQueryHandler(_context, _mapper);
+
+        //Act
+        var result = await handler.Handle(
+            new GetCommentsQuery
+            {
+                From = DateTime.Parse("2008-11-01T19:35:00.0000000Z"),
+                To = DateTime.Parse("2009-11-01T19:35:00.0000000Z")
+            },
+            CancellationToken.None
+        );
+
+        //Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<List<GetCommentsDto>>();
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task GetCommentsQueryHandlerDateFilter_SuccessFound()
+    {
+        //Arrange
+        var handler = new GetCommentsQueryHandler(_context, _mapper);
+
+        //Act
+        var result = await handler.Handle(
+            new GetCommentsQuery
+            {
+                From = DateTime.Today.AddDays(-1),
+                To = DateTime.Today.AddDays(1)
+            },
+            CancellationToken.None
+        );
+
+        //Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<List<GetCommentsDto>>();
+        result.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public async Task GetCommentsQueryHandlerPagination_Success()
+    {
+        //Arrange
+        var handler = new GetCommentsQueryHandler(_context, _mapper);
+
+        //Act
+        var result = await handler.Handle(
+            new GetCommentsQuery
+            {
+                Page = 1,
+                PageAmount = 2
+            },
+            CancellationToken.None
+        );
+
+        //Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<List<GetCommentsDto>>();
+        result.Should().HaveCount(2);
+    }
+
+    [Fact]
     public async Task GetCommentsQueryHandler_NoCommentsFail()
     {
         //Arrange
