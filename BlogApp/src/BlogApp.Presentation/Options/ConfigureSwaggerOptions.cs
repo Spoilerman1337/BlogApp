579fa3d86d57c5280c8954c31df26f8bc3174f85
Maxim Toropygin <mrcatman1337@gmail.com>
2023-02-12 10:54:21 +0300

    reworked add swagger options in separate file in order to comply with asp0000 warning

---
+++ BlogApp/src/BlogApp.Presentation/Options/ConfigureSwaggerOptions.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
+
+namespace BlogApp.Presentation.Options;
+
+public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+{
+    private readonly IApiVersionDescriptionProvider _provider;
+
+    public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => _provider = provider;
+
+    public void Configure(SwaggerGenOptions options)
+    {
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
+
+        foreach (ApiVersionDescription description in _provider.ApiVersionDescriptions)
+        {
+            options.SwaggerDoc(description.GroupName,
+                              new OpenApiInfo()
+                              {
+                                  Title = "BlogAppAPI",
+                                  Version = description.ApiVersion.ToString(),
+                                  Description = $"Blog App API version {description.ApiVersion}"
+                              });
+
+            options.AddSecurityDefinition($"AuthToken {description.ApiVersion}",
+                                         new OpenApiSecurityScheme
+                                         {
+                                             In = ParameterLocation.Header,
+                                             Type = SecuritySchemeType.Http,
+                                             BearerFormat = "JWT",
+                                             Scheme = "bearer",
+                                             Name = "Authorization",
+                                             Description = "Authorization token"
+                                         });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme()
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = $"AuthToken {description.ApiVersion}"
+                        }
+                    },
+                    new string[] {}
+                }
+            });
+
+            options.CustomOperationIds(description =>
+                description.TryGetMethodInfo(out MethodInfo methodInfo) ? methodInfo.Name : null);
+        }
+    }
+}
--- BlogApp/src/BlogApp.Presentation/Program.cs
+++ BlogApp/src/BlogApp.Presentation/Program.cs
@@ -3,10 +3,12 @@ using BlogApp.Application.Common.Interfaces;
 using BlogApp.Domain.Settings;
 using BlogApp.Infrastructure;
 using BlogApp.Presentation.Middleware.CustomExceptionHandler;
+using BlogApp.Presentation.Options;
 using BlogApp.Presentation.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using Serilog;
 using Swashbuckle.AspNetCore.SwaggerGen;
@@ -42,53 +44,8 @@ builder.Services.AddVersionedApiExplorer(options =>
     options.SubstituteApiVersionInUrl = true;
 });
 
-builder.Services.AddSwaggerGen(config =>
-{
-    config.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
-
-    var provider = builder.Services.BuildServiceProvider();
-    var service = provider.GetRequiredService<IApiVersionDescriptionProvider>();
-
-    foreach (ApiVersionDescription description in service.ApiVersionDescriptions)
-    {
-        config.SwaggerDoc(description.GroupName,
-                          new OpenApiInfo()
-                          {
-                              Title = "BlogAppAPI",
-                              Version = description.ApiVersion.ToString(),
-                              Description = $"Blog App API version {description.ApiVersion}"
-                          });
-
-        config.AddSecurityDefinition($"AuthToken {description.ApiVersion}",
-                                     new OpenApiSecurityScheme
-                                     {
-                                         In = ParameterLocation.Header,
-                                         Type = SecuritySchemeType.Http,
-                                         BearerFormat = "JWT",
-                                         Scheme = "bearer",
-                                         Name = "Authorization",
-                                         Description = "Authorization token"
-                                     });
-
-        config.AddSecurityRequirement(new OpenApiSecurityRequirement
-        {
-            {
-                new OpenApiSecurityScheme()
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = $"AuthToken {description.ApiVersion}"
-                    }
-                },
-                new string[] {}
-            }
-        });
-
-        config.CustomOperationIds(description =>
-            description.TryGetMethodInfo(out MethodInfo methodInfo) ? methodInfo.Name : null);
-    }
-});
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 
 builder.Services.AddApiVersioning(config =>
 {
@@ -128,8 +85,7 @@ app.UseSwagger(config =>
 });
 app.UseSwaggerUI(config =>
 {
-    var provider = builder.Services.BuildServiceProvider();
-    var service = provider.GetRequiredService<IApiVersionDescriptionProvider>();
+    var service = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
     foreach (ApiVersionDescription description in service.ApiVersionDescriptions)
     {
         config.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
