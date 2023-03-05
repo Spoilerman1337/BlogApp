a63e0788f723d43f6c71ec601992d8494c26ff04
Maxim Toropygin <mrcatman1337@gmail.com>
2023-03-05 21:58:49 +0300

    prettier config update and code reformatting

--- BlogApp.Frontend/src/index.tsx
+++ BlogApp.Frontend/src/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "../App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '../App';
 
-const root = document.getElementById("root");
-if (root === null) throw new Error("Root container missing in index.html");
+const root = document.getElementById('root');
+if (root === null) throw new Error('Root container missing in index.html');
 
 const rootRender = ReactDOM.createRoot(root);
 const element = (
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 rootRender.render(element);
