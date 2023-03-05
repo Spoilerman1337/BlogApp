bc8c83ea61cc6f63e874f79a319776bbab33c3d9
Maxim Toropygin <mrcatman1337@gmail.com>
2023-03-05 10:47:49 +0300

    prettier installed and configured

--- BlogApp.Frontend/App.tsx
+++ BlogApp.Frontend/App.tsx
@@ -2,13 +2,13 @@ import React, { FC, ReactElement } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App: FC<object> = (): ReactElement => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<h1>Home</h1>} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- BlogApp.Frontend/webpack.config.js
+++ BlogApp.Frontend/webpack.config.js
@@ -1,82 +1,82 @@
-const path = require('path');
+const path = require("path");
 
-const prod = process.env.NODE_ENV === 'production';
+const prod = process.env.NODE_ENV === "production";
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: prod ? 'production' : 'development',
-    entry: ['@babel/polyfill', './src/index.tsx'],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env']
-                }
-              }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            hmr: !prod,
-                            reloadAll: true
-                        }
-                    },
-                    'css-loader'
-                ],
-            },
-            {
-                test: /\.s[ac]ss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            hmr: !prod,
-                            reloadAll: true
-                        }
-                    },
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                use: 'ts-loader',
-            },
-          ]
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json'],
-      },
-    devServer: {
-        static: {
-          directory: path.join(__dirname, 'public'),
+  mode: prod ? "production" : "development",
+  entry: ["@babel/polyfill", "./src/index.tsx"],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
-        compress: true,
-        port: 5000,
       },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].bundle.css'
-        })
-    ]
-}
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: !prod,
+              reloadAll: true,
+            },
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: !prod,
+              reloadAll: true,
+            },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 5000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].bundle.css",
+    }),
+  ],
+};
